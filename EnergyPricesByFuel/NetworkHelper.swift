0cd7bb23d9d6305e8472bdf9f7a84057efc042c5
Mei <meitao10@gmail.com>
2016-04-23 14:06:47 -0400

    units

--- EnergyPricesByFuel/NetworkHelper.swift
+++ EnergyPricesByFuel/NetworkHelper.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 protocol NetworkHelperDelegate {
     func didReceiveprices(gas: Fuel, oil: Fuel)
@@ -18,6 +19,13 @@ class NetworkHelper {
     var oilPrice: Float?
     
     var delegate: NetworkHelperDelegate?
+    
+    var currentBrentLink = String("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.quote%20WHERE%20symbol%3D%22BZM16.NYM%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
+    
+    var currentHHLink = String("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.quote%20WHERE%20symbol%3D%22NGK16.NYM%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
+    
+    let eiaLinkBeg = String("https://api.eia.gov/series/?api_key=6495EB74AFCC6C24FFB6500DC2B9AB44&series_id=NG.N3050")
+    let eiaLinkEnd = String("3.M")
     
     func loadOilPrice() {
         // Get Yahoo Finance data for Brent with a query link
@@ -58,20 +66,12 @@ class NetworkHelper {
     
     func checkPricesAvailability() {
         if let gasPrice = gasPrice, oilPrice = oilPrice {
-            oil.variableCostWithFuel = oilPrice
-            gas.variableCostWithFuel = gasPrice
+            oil.variableCostWithFuel = oilPrice / bblToMwh
+            gas.variableCostWithFuel = gasPrice / mcfToMwh
             
             delegate?.didReceiveprices(gas, oil: oil)
         }
     }
-    
-    var currentBrentLink = String("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.quote%20WHERE%20symbol%3D%22BZM16.NYM%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
-    
-    var currentHHLink = String("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.quote%20WHERE%20symbol%3D%22NGK16.NYM%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
-    
-    let eiaLinkBeg = String("https://api.eia.gov/series/?api_key=6495EB74AFCC6C24FFB6500DC2B9AB44&series_id=NG.N3050")
-    let eiaLinkEnd = String("3.M")
-    
     
     func parseYFPriceWithJSON(data: NSDictionary) -> Float? {
         // parse Yahoo finance data and return a Brent or Henry Hub price
@@ -81,7 +81,7 @@ class NetworkHelper {
             let lastPrice = quote["LastTradePriceOnly"] as? String else{
                 return nil
         }
-        print(lastPrice)
+        // print(lastPrice)
         return Float(lastPrice)
     }
     
@@ -95,7 +95,7 @@ class NetworkHelper {
             else {
                 return nil
         }
-        print(latestGasPrice)
+        // print(latestGasPrice)
         return latestGasPrice
     }
     
@@ -104,24 +104,27 @@ class NetworkHelper {
 
 }
 
-//Wind
+// Wind
 var wind = Fuel(fuelType: "Wind", levelizedCapitalCost: 58, fixedCost: 13, variableCostWithFuel: 0, transmissionInvestment: 3)
 
-//Solar PV
+// Solar PV
 var solar = Fuel(fuelType: "Solar PV", levelizedCapitalCost: 110, fixedCost: 11, variableCostWithFuel: 0, transmissionInvestment: 4)
 
-//Hydroelectric
+// Hydroelectric
 var hydro = Fuel(fuelType: "Hydro", levelizedCapitalCost: 71, fixedCost: 4, variableCostWithFuel: 7, transmissionInvestment: 2)
 
 // Conventional Coal
 var coal = Fuel(fuelType: "Coal", levelizedCapitalCost: 60, fixedCost: 4, variableCostWithFuel: 29, transmissionInvestment: 1)
 
 // Natural Gas-fired Conventional Combined Cycle
-// 1 megawatt hours = 3.41214 mcf (thousand cubic feet) of natural gas
 var gas = Fuel(fuelType: "Natural Gas", levelizedCapitalCost: 14, fixedCost: 2, variableCostWithFuel: 57, transmissionInvestment: 1)
+// 1 megawatt hours = 3.41214 mcf (thousand cubic feet) of natural gas
+let mcfToMwh = Float(1/3.41214)
 
 // Conventional Combustion Turbine
 var oil = Fuel(fuelType: "Oil", levelizedCapitalCost: 41, fixedCost: 3, variableCostWithFuel: 45, transmissionInvestment: 4)
+// 1 boe = 1.6282 Mwh
+let bblToMwh = Float(1.6282)
 
 // Advanced Nuclear
 var nuclear = Fuel(fuelType: "Nuclear", levelizedCapitalCost: 70, fixedCost: 12, variableCostWithFuel: 12, transmissionInvestment: 1)
